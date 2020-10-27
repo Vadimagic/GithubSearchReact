c1c81c89d28bd5ce3d8201a12cacda282f739314
Vadim <vadim.prog.2000@gmail.com>
2020-10-27 12:46:31 +0300

    create pages, search, card, alert, navbar and context hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Alert } from './components/Alert';
+import { Navbar } from "./components/Navbar";
+import { AlertState } from './context/alert/alertState';
+import { About } from './pages/About';
+import { Home } from "./pages/Home";
+import { Profile } from './pages/Profile';
+
 function App() {
 	return (
-		<>
-			<div className="container pt-4">
-				<h1>Hello World!</h1>
-			</div>
-		</>
+		<AlertState>
+			<BrowserRouter>
+				<Navbar/>
+				<div className="container pt-4">
+					<Alert alert={{text: 'test alert'}}/>
+					<Switch>
+						<Route path="/" exact component={Home}/>
+						<Route path="/about" component={About}/>
+						<Route path="/profile/:name" component={Profile}/>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</AlertState>
 	);
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+export const About = () => {
+	return (
+		<div className="jumbotron jumbotron-fluid">
+			<div className="container">
+				<h1 className="display-4">Информация</h1>
+				<p className="lead">Версия приложения <strong>1.9.7</strong></p>
+			</div>
+		</div>
+	)
+}
